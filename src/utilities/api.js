78e1d8336287345423001c57db29ef9795e488e6
Brian <brian.kiprono8@student.moringaschool.com>
2025-04-21 02:19:19 +0300

    fetch api

---
+++ src/utilities/api.js
@@ -0,0 +1,13 @@
+export const fetchMusic = async (term) => {
+    const URL = `https://itunes.apple.com/search?term=${encodeURIComponent(term)}&media=music&entity=musicTrack&limit=10`;
+
+    try{
+        const response = await fetch(URL).
+        const data = await response.json();
+        console.log(data);        
+    }
+    catch(error){
+        console.error("Failed to Fetch Music!!")
+    }
+    
+}
