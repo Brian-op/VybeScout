0b1a86fa09dc58b6e0d1d2ea5d18eb04a8af5ce3
Brian <brian.kiprono8@student.moringaschool.com>
2025-04-21 03:02:43 +0300

    update api

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,13 +1,14 @@
 export const fetchMusic = async (term) => {
-    const URL = `https://itunes.apple.com/search?term=${encodeURIComponent(term)}&media=music&entity=musicTrack&limit=10`;
+    const URL = `https://itunes.apple.com/search?term=${encodeURIComponent(term)}&media=music&entity=musicTrack&limit=200`;
 
     try{
         const response = await fetch(URL);
         const data = await response.json();
-        console.log(data);        
+        return data.results;              
     }
     catch(error){
-        console.error("Failed to Fetch Music!!")
+        console.error("Failed to Fetch Music!!",error)
+        return[];
     }
     
 }
